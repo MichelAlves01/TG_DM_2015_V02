7ff7f9d0ba7c4b04366dd5ed7714725a7b6e7162
unknown <micheldejesus@gmail.com>
2015-08-08 23:34:04 -0300

    Mobile get empresas Lat long | 50%

--- DeliveryMobile/www/angular/controller/principalMobile.js
+++ DeliveryMobile/www/angular/controller/principalMobile.js
@@ -1,9 +1,9 @@
 (function(){
 	var app = angular.module('principalMobile' , ['usuarioMobile','mobileDirectives']);
-    
+    var urlBase = 'http://192.168.0.11:8080';
     
     app.controller('principalCtrl' , function($scope,$http){
-       
+       $http.defaults.headers.post["Content-Type"] = "application/jsonp";
         $scope.showProdutos = function(produtoTipo){
             $("#containerPrincipal").hide();
             $("#containerUsuario").hide();
@@ -12,5 +12,13 @@
                  
             }
         };
+        
+        $scope.getEmpresasProdutosController = function(){
+            var data = $.param({latitude: -23.4534596 , longitude: -47.4900411});
+            $http.get(urlBase + '/getEmpresasPorLatLong?'+ data).success(function( data , status){
+                $scope.empresas = data;
+                alert( $scope.empresas[0].nome);
+            });
+        }
     });
 })();
